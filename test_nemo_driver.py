0173ed9d4d6e72efe90920de4637eb5564050cac
gunho <qlfydehd@gmail.com>
2024-07-12 11:28:52 +0900

    test_nemo_driver.py TC 추가

--- test_nemo_driver.py
+++ test_nemo_driver.py
@@ -3,13 +3,50 @@ from unittest import TestCase
 
 class TestNemoDriver(TestCase):
     def test_nemo_login(self):
-        pass
+        output = io.StringIO()
+        original_stdout = sys.stdout
+        sys.stdout = output
+
+        try:
+            stock_dirver = StockBroker(NemoDriver())
+            stock_driver.login()
+
+        finally:
+            sys.stdout = original_stdout
+
+        catured_output = output.getvalue()
+        self.assertGreaterEqual(0, captured_output.lower().find("login"))
 
     def test_nemo_buy(self):
-        pass
+        output = io.StringIO()
+        original_stdout = sys.stdout
+        sys.stdout = output
 
-    def test_nemo_selly(self):
-        pass
+        try:
+            stock_dirver = StockBroker(NemoDriver())
+            stock_driver.buy(222, 1000, 10)
+
+        finally:
+            sys.stdout = original_stdout
+
+        catured_output = output.getvalue()
+        self.assertGreaterEqual(0, captured_output.lower().find("buy"))
+
+    def test_nemo_sell(self):
+        output = io.StringIO()
+        original_stdout = sys.stdout
+        sys.stdout = output
+
+        try:
+            stock_dirver = StockBroker(NemoDriver())
+            stock_driver.sell(222, 1000, 10)
+
+        finally:
+            sys.stdout = original_stdout
+
+        catured_output = output.getvalue()
+        self.assertGreaterEqual(0, captured_output.lower().find("sell"))
 
     def test_nemo_get_price(self):
-        pass
+        stock_dirver = StockBroker(NemoDriver())
+        self.assertGreaterEqual(0, stock_driver.get_price(222))
