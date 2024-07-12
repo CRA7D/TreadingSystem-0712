8b1b6b66dd0a44ae31b29a770abbabf1b3e0d641
h.seok <h.seoK@samsung.com>
2024-07-12 16:28:49 +0900

    stockaccount complete

--- stock_account.py
+++ stock_account.py
@@ -5,16 +5,19 @@ from stockbroker import StockBroker
 
 class StockAccount:
     def __init__(self, stock_broker: StockBroker):
-        self.code_and_amount = defaultdict(int)
-        self.cash = 100000
-        self.stock_broker = StockBroker
+        self.stock_list = defaultdict(int)
+        self.cash = 1000000
+        self.stock_broker = stock_broker
+
+    def cnt_of_stock_class(self):
+        return len(self.stock_list)
 
     def get_total_asset(self):
         return self.cash + self.get_total_stock_amount()
 
     def get_total_stock_amount(self):
         result = 0
-        for code, amount in self.code_and_amount.items():
+        for code, amount in self.stock_list.items():
             result += self.stock_broker.get_price(code) * amount
         return result
 
@@ -22,12 +25,21 @@ class StockAccount:
         return self.cash
 
     def get_amount_by(self, code):
-        return self.code_and_amount[code]
+        return self.stock_list[code]
 
-    def add_stock(self, code, amount):
-        self.code_and_amount[code] += amount
+    def add_stock(self, code, price, amount):
+        if price * amount > self.cash:
+            return False
+        self.stock_list[code] += amount
+        self.cash -= price * amount
+        return True
 
-    def decrease_stock(self, code, amount):
-        self.code_and_amount[code] -= amount
+    def del_stock(self, code, amount):
+        if self.stock_list[code] < amount:
+            return False
+        self.stock_list[code] -= amount
+        self.cash += self.stock_broker.get_price(code) * amount
+        return True
 
-
+    def get_current_stock_price(self, code):
+        return self.stock_broker.get_price(code) * self.stock_list[code]
--- test_stock_account.py
+++ test_stock_account.py
@@ -1,47 +1,44 @@
 from unittest import TestCase
 from unittest.mock import Mock
 
+from stock_account import StockAccount
 from stockbroker import StockBroker
 
 
 class TestStockAccount(TestCase):
-    def test_buy_stock(self):
-        sut = StockAccount()
-        sut.add_stock("001234", 2000, 3)
-        sut.add_stock("003456", 3000, 2)
 
-        self.assertEqual(2, sut.stock_list)
+    def setUp(self):
+        stock_broker = Mock()
+        stock_broker.get_price.return_value = 100
+        self.sut = StockAccount(stock_broker)
+    def test_buy_stock(self):
+        self.sut.add_stock("001234", 2000, 3)
+        self.sut.add_stock("003456", 3000, 2)
+
+        self.assertEqual(2, self.sut.cnt_of_stock_class())
 
     def test_buy_stock_same_product(self):
-        sut = StockAccount()
-        sut.add_stock("003344", 2000, 3)
-        sut.add_stock("003344", 2000, 3)
+        self.sut.add_stock("003344", 2000, 3)
+        self.sut.add_stock("003344", 2000, 3)
 
-        self.assertEqual(1, sut.stock_list)
-        self.assertEqual(6, sut.stock_list["003344"]["cnt"])
+        self.assertEqual(1, self.sut.cnt_of_stock_class())
+        self.assertEqual(6, self.sut.stock_list["003344"])
 
     def test_sell_stock_normal(self):
-        sut = StockAccount()
-        sut.add_stock("003344", 2000, 3)
+        self.sut.add_stock("003344", 2000, 3)
 
-        self.assertTrue(sut.del_stock("003344", 2))
+        self.assertTrue(self.sut.del_stock("003344", 2))
 
     def test_sell_stock_more_than_count(self):
-        sut = StockAccount()
-        sut.add_stock("003344", 2000, 3)
-        self.assertFalse(sut.del_stock("003344", 4))
+        self.sut.add_stock("003344", 2000, 3)
+        self.assertFalse(self.sut.del_stock("003344", 4))
 
     def test_get_current_total_price_when_no_stock(self):
-        sut = StockAccount()
-        self.assertEqual(1000000, sut.get_total_price())
+        self.assertEqual(1000000, self.sut.get_cash())
 
     def test_get_current_total_price_when_one_stock(self):
-        sut = StockAccount()
-        stb = Mock(spec=StockBroker())
-        stb.get_price.return_value = 3000
+        self.sut.add_stock("003344", 2000, 3)
 
-        sut.add_stock("003344", 2000, 3)
-
-        self.assertEqual(9000, sut.get_current_stock_price("003344"))
+        self.assertEqual(300, self.sut.get_current_stock_price("003344"))
 
 
