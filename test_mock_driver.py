79f108e88b06d019df746020b79388bd9c254b3a
gunho <qlfydehd@gmail.com>
2024-07-12 10:46:37 +0900

    test_mock_driver.py TC 추가

--- test_mock_driver.py
+++ test_mock_driver.py
@@ -2,4 +2,14 @@ from unittest import TestCase
 
 
 class TestMockDriver(TestCase):
-    pass
+    def test_mock_login(self):
+        pass
+
+    def test_mock_buy(self):
+        pass
+
+    def test_mock_selly(self):
+        pass
+
+    def test_mock_get_price(self):
+        pass
