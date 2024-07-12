c25460f3bdff92924a3c764a60823ea748257ccf
gunho <qlfydehd@gmail.com>
2024-07-12 10:43:34 +0900

    test_auto_trading_system.py tc 추가

--- test_auto_trading_system.py
+++ test_auto_trading_system.py
@@ -2,4 +2,23 @@ from unittest import TestCase
 
 
 class TestAutoTradingSystem(TestCase):
-    pass
+    def test_select_stock_brocker_kiwer선택(self):
+        pass
+
+    def test_select_stock_brocker_nemo선택(self):
+        pass
+
+    def test_select_stock_brocker_mock선택(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_buy(self):
+        pass
+
+    def test_sell(self):
+        pass
+
+    def test_get_price(self):
+        pass
