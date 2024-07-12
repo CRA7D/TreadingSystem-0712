1b42aff1c2f044009e976b663e57aa4ec386ae1f
gunho <qlfydehd@gmail.com>
2024-07-12 11:17:15 +0900

    test_auto_trading_system.py tc 추가

--- test_auto_trading_system.py
+++ test_auto_trading_system.py
@@ -1,24 +1,73 @@
+import io
+import sys
 from unittest import TestCase
+
+from stockbroker import StockBroker
 
 
 class TestAutoTradingSystem(TestCase):
     def test_select_stock_brocker_kiwer선택(self):
-        pass
+        stock_driver = StockBroker(KiwerDriver())
+        self.assertEqual(isinstance(stock_driver), KiwerDriver)
 
     def test_select_stock_brocker_nemo선택(self):
-        pass
+        stock_driver = StockBroker(KiwerDriver())
+        self.assertEqual(isinstance(stock_driver), NemorDriver)
 
     def test_select_stock_brocker_mock선택(self):
-        pass
+        stock_driver = StockBroker(MockDriver())
+        self.assertEqual(isinstance(stock_driver), MockDriver)
 
     def test_login(self):
-        pass
+        output = io.StringIO()
+        original_stdout = sys.stdout
+        sys.stdout = output
+
+        try:
+            stock_dirver = StockBroker(MockDriver())
+            stock_driver.login()
+
+        finally:
+            sys.stdout = original_stdout
+
+        catured_output = output.getvalue()
+        self.assertGreaterEqual(0,captured_output.lower().find("login"))
+
 
     def test_buy(self):
-        pass
+        output = io.StringIO()
+        original_stdout = sys.stdout
+        sys.stdout = output
+
+        try:
+            stock_dirver = StockBroker(MockDriver())
+            stock_driver.buy(222, 1000, 10)
+
+        finally:
+            sys.stdout = original_stdout
+
+        catured_output = output.getvalue()
+        self.assertGreaterEqual(0, captured_output.lower().find("buy"))
 
     def test_sell(self):
-        pass
+        output = io.StringIO()
+        original_stdout = sys.stdout
+        sys.stdout = output
+
+        try:
+            stock_dirver = StockBroker(MockDriver())
+            stock_driver.sell(222, 1000, 10)
+
+        finally:
+            sys.stdout = original_stdout
+
+        catured_output = output.getvalue()
+        self.assertGreaterEqual(0, captured_output.lower().find("sell"))
 
     def test_get_price(self):
-        pass
+
+        stock_dirver = StockBroker(MockDriver())
+
+
+
+        self.assertGreaterEqual(0, stock_driver.get_price(222))
