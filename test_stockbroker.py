964ad40b8badecffcd92044631d61925c13e106c
yj93.cho <bigeye9312@gmail.com>
2024-07-12 15:46:09 +0900

    [feature] use get_price to call private method for validation

--- test_stockbroker.py
+++ test_stockbroker.py
@@ -1,24 +1,25 @@
 from unittest import TestCase
+from unittest.mock import Mock
 
 from stockbroker import StockBroker
 
 
 class TestStockBroker(TestCase):
-    def test_input_invalid_code_when(self):
-        self.sut = StockBroker()
+    def setUp(self):
+        mock = Mock()
+        self.sut = StockBroker(mock)
 
+    def test_input_invalid_code_when(self):
         with self.assertRaises(Exception):
-            self.sut.__invalid_check_code("123")
-            self.sut.__invalid_check_code("D001234")
-            self.sut.__invalid_check_code("A00123C")
-            self.sut.__invalid_check_code("A0013234")
+            self.sut.get_price("123")
+            self.sut.get_price("D001234")
+            self.sut.get_price("A00123C")
+            self.sut.get_price("A0013234")
 
     def test_input_valid_code(self):
-        self.sut = StockBroker()
-
         try:
-            self.sut.__invalid_check_code("001234")
-            self.sut.__invalid_check_code("A004562")
-            self.sut.__invalid_check_code("C004562")
+            self.sut.get_price("001234")
+            self.sut.get_price("A004562")
+            self.sut.get_price("C004562")
         except Exception:
             self.fail()
