bc2c4057eb794cc0701da2fd1da7d96a0a05e340
gunho <qlfydehd@gmail.com>
2024-07-12 10:45:17 +0900

    test_kiwer_driver.py TC 추가

--- test_kiwer_driver.py
+++ test_kiwer_driver.py
@@ -2,4 +2,12 @@ from unittest import TestCase
 
 
 class TestKiwerDriver(TestCase):
-    pass
+    def test_kiwer_login(self):
+        pass
+
+    def test_kiwer_buy(self):
+        pass
+    def test_kiwer_selly(self):
+        pass
+    def test_kiwer_get_price(self):
+        pass
